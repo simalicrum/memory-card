adc9c70e7d9c61d22f7c923fa6e8be9ab07859cc
Robert Reinert <robert.reinert@gmail.com>
2020-10-09 14:49:18 -0700

    added photos

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ import Card from "./Card";
 
 const App = () => {
   const [cats, setCats] = useState([
-    "Yin",
-    "Yang",
-    "Punkle",
-    "Mr. Black",
-    "Mr. Grey",
+    { name: "Ann", picture: "Ann.jpg" },
+    { name: "Daisy", picture: "Daisy.jpg" },
+    { name: "Juniper", picture: "Juniper.jpg" },
+    { name: "Koshenya", picture: "Koshenya.jpg" },
+    { name: "Pebbles", picture: "Pebbles.jpg" },
+    { name: "Punkle", picture: "Punkle.JPG" },
+    { name: "Storm", picture: "Storm.jpg" },
+    { name: "Supurr", picture: "Supurr.jpg" },
+    { name: "Yang", picture: "Yang.jpg" },
+    { name: "Yin", picture: "Yin.jpg" },
   ]);
   const [clickedCats, setClickedCats] = useState([]);
   const [score, setScore] = useState(0);
@@ -52,11 +57,11 @@ const App = () => {
       return (
         <div
           onClick={() => {
-            checkClicks(i);
+            checkClicks(i.name);
             randomizeCats();
           }}
         >
-          <Card catname={i} />
+          <Card catname={i.name} catpicture={i.picture} />
         </div>
       );
     });
@@ -64,7 +69,7 @@ const App = () => {
   return (
     <div>
       <Header score={score} highScore={highScore} />
-      {renderCats()}
+      <div id="cats">{renderCats()}</div>
     </div>
   );
 };
--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./Card.css";
 
 const Card = (props) => {
-  return <div>{props.catname}</div>;
+  return (
+    <div className="card">
+      <img className="cat-picture" src={props.catpicture} alt={props.catname} />
+    </div>
+  );
 };
 
 export default Card;
