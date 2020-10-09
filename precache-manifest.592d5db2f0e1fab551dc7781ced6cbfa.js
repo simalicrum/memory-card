fc87cf491ca76f51a4d8fbb678fbfbef734957b1
Robert Reinert <robert.reinert@gmail.com>
2020-10-09 14:55:08 -0700

    Updates

---
+++ precache-manifest.592d5db2f0e1fab551dc7781ced6cbfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f5add10ab879b33b530c86af00675a",
+    "url": "/memory-card/index.html"
+  },
+  {
+    "revision": "253387a1e409d0d05535",
+    "url": "/memory-card/static/css/main.f443b4a1.chunk.css"
+  },
+  {
+    "revision": "152b64a1566a95710f59",
+    "url": "/memory-card/static/js/2.22a3c0a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-card/static/js/2.22a3c0a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "253387a1e409d0d05535",
+    "url": "/memory-card/static/js/main.942942fd.chunk.js"
+  },
+  {
+    "revision": "2d10a56d9bedc8f13787",
+    "url": "/memory-card/static/js/runtime-main.4cf988ce.js"
+  }
+]);
