dabcecae55605407544900f19b4684ff520e81c4
Robert Reinert <robert.reinert@gmail.com>
2020-10-08 21:58:51 -0700

    Initial commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Cat Memory Game</h1>
+      <h2>Current Score: 0</h2>
+      <h2>High Score : 0</h2>
+    </div>
+  );
+};
+
+export default Header;
