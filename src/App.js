a0797c948b1dda5d8c9bf422ed2d7213c62dbf54
Robert Reinert <robert.reinert@gmail.com>
2020-10-09 13:21:20 -0700

    Game logic complete

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ const App = () => {
     "Mr. Black",
     "Mr. Grey",
   ]);
-
+  const [clickedCats, setClickedCats] = useState([]);
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
   const randomizeRange = (length) => {
     let numbers = [Math.floor(Math.random() * length)];
     let j = Math.floor(Math.random() * length);
@@ -21,10 +23,11 @@ const App = () => {
       }
       numbers.push(j);
     }
-
     return numbers;
   };
-
+  useEffect(() => {
+    randomizeCats();
+  }, []);
   const randomizeCats = () => {
     setCats(randomizeArray(cats));
   };
@@ -32,23 +35,36 @@ const App = () => {
     const randomIndex = randomizeRange(array.length);
     return randomIndex.map((i) => array[i]);
   };
-
+  const checkClicks = (cat) => {
+    console.log(cat);
+    if (clickedCats.some((i) => cat === i)) {
+      setClickedCats([]);
+      if (score > highScore) {
+        setHighScore(score);
+      }
+      setScore(0);
+    } else {
+      setClickedCats(clickedCats.concat(cat));
+      setScore(score + 1);
+    }
+  };
   const renderCats = () => {
-    return randomizeRange(cats.length).map((i) => {
+    return cats.map((i) => {
       return (
         <div
           onClick={() => {
+            checkClicks(i);
             randomizeCats();
           }}
         >
-          <Card catname={cats[i]} />
+          <Card catname={i} />
         </div>
       );
     });
   };
   return (
     <div>
-      <Header />
+      <Header score={score} highScore={highScore} />
       {renderCats()}
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <div>
       <h1>Cat Memory Game</h1>
-      <h2>Current Score: 0</h2>
-      <h2>High Score : 0</h2>
+      <h2>Current Score: {props.score}</h2>
+      <h2>High Score : {props.highScore}</h2>
     </div>
   );
 };
